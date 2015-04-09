7430af96089b2927c114d15788985265d12d7882
David Lindblad <david.lindblad@hotmail.com>
2015-04-09 21:54:27 +0200

    init hello world

---
+++ HelloWorld/HelloWorldGrainInterfaces/IHelloGrain.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Orleans;
+
+namespace HelloWorldGrainInterfaces
+{
+    /// <summary>
+    /// Grain interface IHelloGrain
+    /// </summary>
+    public interface IHelloGrain : IGrainWithIntegerKey
+    {
+        
+    }
+}
---
+++ HelloWorld/HelloWorldGrains/HelloGrain.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using HelloWorldGrainInterfaces;
+using Orleans;
+
+namespace HelloWorldGrains
+{
+    /// <summary>
+    /// Grain implementation class HelloGrain.
+    /// </summary>
+    public class HelloGrain : Grain, IHelloGrain
+    {
+        
+    }
+}
